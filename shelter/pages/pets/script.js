2820cec0cc4333a7b3a110eed0ade4b37bf5aa74
Artur Bazaluk <52492191+turik777@users.noreply.github.com>
2024-08-20 16:12:10 +0300

    feat: add burger menu functionality

---
+++ shelter/pages/pets/script.js
@@ -0,0 +1,24 @@
+const burger = document.querySelector(".burger-icon");
+const menu = document.querySelector(".menu");
+const menuBackground = document.querySelector(".menu-background");
+const menuLinks = document.querySelectorAll(".menu-link");
+
+function showMenu() {
+    if (menu.classList.contains("menu-active")) {
+        menuBackground.style.backgroundColor = "transparent";
+        setTimeout(() => menuBackground.classList.remove("menu-background-active"), 500);
+    } else {
+        menuBackground.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
+        menuBackground.classList.add("menu-background-active");
+    }
+
+    burger.classList.toggle("burger-icon-active");
+    menu.classList.toggle("menu-active");
+    document.querySelector("html").classList.toggle("overflow-hidden");
+}
+
+burger.addEventListener("click", showMenu);
+menuBackground.addEventListener("click", showMenu);
+menuLinks.forEach((link) => {
+    link.addEventListener("click", showMenu);
+})

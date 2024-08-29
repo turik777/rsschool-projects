5622349f7b9abe0a4bc800ba06e6a0d23ad874fd
Artur Bazaluk <52492191+turik777@users.noreply.github.com>
2024-08-30 01:49:03 +0300

    feat: implement logic for random generator

--- shelter/pages/pets/script.js
+++ shelter/pages/pets/script.js
@@ -24,3 +24,30 @@ menuBackground.addEventListener("click", showMenu);
 menuLinks.forEach((link) => {
     link.addEventListener("click", showMenu);
 })
+
+let arrayPets = [];
+let arrayRandom = [];
+
+function randomArr() {
+    arrayRandom = [];
+    
+    while (arrayRandom.length < 8) {
+        let random = Math.floor(Math.random() * 8);
+        if (arrayRandom.indexOf(random) === -1) {
+            arrayRandom.push(random);
+        }
+    }  
+}
+randomArr();
+
+arrayPets.push(arrayRandom);
+let last = [arrayRandom[6], arrayRandom[7]];
+
+while (arrayPets.length < 6) {
+    randomArr();
+    while (arrayRandom.slice(0, 4).some(x => last.includes(x))) {
+        randomArr();
+    }
+    arrayPets.push(arrayRandom);
+    last = [arrayRandom[6], arrayRandom[7]];
+}

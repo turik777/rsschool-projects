57ed0099e9ef4a1ebdab8c0767bcda19ce1c9783
Artur Bazaluk <52492191+turik777@users.noreply.github.com>
2024-09-06 22:32:18 +0300

    feat: implement logic for cards render and random generator

--- shelter/pages/main/script.js
+++ shelter/pages/main/script.js
@@ -24,3 +24,70 @@ menuBackground.addEventListener("click", showMenu);
 menuLinks.forEach((link) => {
     link.addEventListener("click", showMenu);
 })
+
+function renderCard(pet, number) {
+    const cards = document.querySelectorAll(".pets-cards");
+    const card = document.createElement("div");
+    const cardImage = document.createElement("img");
+    const cardTitle = document.createElement("p");
+    const cardButton = document.createElement("button");
+
+    card.classList.add("pets-card");
+    cardImage.classList.add("pets-card-title");
+    cardTitle.classList.add("pets-card-title");
+    cardButton.classList.add("pets-card-button");
+
+    cardImage.src = pet.img;
+    cardImage.alt = pet.type + " " + pet.name;
+    cardTitle.textContent = pet.name;
+    cardButton.textContent = "Learn more";
+
+    cards[number].appendChild(card);
+    card.appendChild(cardImage);
+    card.appendChild(cardTitle);
+    card.appendChild(cardButton);
+
+    card.addEventListener("click", () => {
+        showPopup(pet);
+    });
+}
+
+let arrayCurrRandom = [];
+let arrayNextRandom = [];
+
+function randomArr() {
+    arrayCurrRandom = [];
+    
+    while (arrayCurrRandom.length < 3) {
+        let random = Math.floor(Math.random() * 8);
+        if (arrayCurrRandom.indexOf(random) === -1) {
+            arrayCurrRandom.push(random);
+        }
+    }  
+}
+
+function randomNextArr() {
+    arrayNextRandom = [];
+    
+    while (arrayNextRandom.length < 3) {
+        let random = Math.floor(Math.random() * 8);
+        if (arrayCurrRandom.indexOf(random) === -1 && arrayNextRandom.indexOf(random) === -1) {
+            arrayNextRandom.push(random);
+        }
+    }  
+}
+
+randomArr();
+arrayCurrRandom.forEach(pet => {
+    renderCard(pets[pet], 1);
+});
+
+randomNextArr();
+arrayNextRandom.forEach(pet => {
+    renderCard(pets[pet], 0);
+});
+
+randomNextArr();
+arrayNextRandom.forEach(pet => {
+    renderCard(pets[pet], 2);
+});

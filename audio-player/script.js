2cdb3fe52fe8cf0c1f4f3ad52f88a3e5a6c3ab65
Artur Bazaluk <52492191+turik777@users.noreply.github.com>
2024-09-18 04:58:20 +0300

    feat: add forward/backward buttons implementations

--- audio-player/script.js
+++ audio-player/script.js
@@ -16,3 +16,49 @@ play.addEventListener("click", () => {
         audio[0].pause();
     }
 });
+
+const title = document.querySelector(".song-title");
+const artist = document.querySelector(".song-artist");
+const cover = document.querySelector(".song-cover");
+
+let trackCount = Math.floor(Math.random() * tracks.length);
+function renderTrack() {
+    audio[0].src = `assets/audio/${tracks[trackCount].title.toLowerCase().split(" ").join("-")}.mp3`;
+    title.textContent = `${tracks[trackCount].title}`;
+    artist.textContent = `${tracks[trackCount].artist}`;
+    cover.style.background = `url(assets/img/${tracks[trackCount].cover}.png) center / cover`;
+}
+renderTrack();
+
+const backward = document.querySelector(".backward");
+const forward = document.querySelector(".forward");
+
+function previousTrack() {
+    trackCount--;
+    if (trackCount < 0) {
+        trackCount = tracks.length - 1;
+    }
+    renderTrack();
+
+    if (play.classList.contains("pause")) {
+        audio[0].play();
+    } else {
+        audio[0].pause();
+    }
+}
+backward.addEventListener("click", previousTrack);
+
+function nextTrack() {
+    trackCount++;
+    if (trackCount >= tracks.length) {
+        trackCount = 0;
+    }
+    renderTrack();
+
+    if (play.classList.contains("pause")) {
+        audio[0].play();
+    } else {
+        audio[0].pause();
+    }
+}
+forward.addEventListener("click", nextTrack);

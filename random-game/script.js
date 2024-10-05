0b7e828240135d9683b663669683f373a2b5a0ea
Artur Bazaluk <52492191+turik777@users.noreply.github.com>
2024-10-05 19:02:50 +0300

    feat: add function for match check

--- random-game/script.js
+++ random-game/script.js
@@ -23,6 +23,36 @@ cards.forEach(card => {
             firstCard = card;
         } else {
             secondCard = card;
+            checkMatch(firstCard, secondCard);
         }
     });
 });
+
+let matched = 0;
+let moves = 0;
+function checkMatch(first, second) {
+    moves++;
+    if (first.children[1].children[0].src !== second.children[1].children[0].src) {
+        setTimeout(() => {
+            first.classList.remove("flip");
+            second.classList.remove("flip");
+        }, 1000);
+    } else {
+        matched++;
+    }
+
+    firstCard = undefined;
+    secondCard = undefined;
+
+    if (matched === 10) {
+        setTimeout(() => {
+            alert(`You win! Your number of moves: ${moves}`);
+        }, 1000);
+        setTimeout(() => {
+            cards.forEach(card => card.classList.remove("flip"));
+            matched = 0;
+            moves = 0;
+         }, 2000);
+         setTimeout(() => shuffleCards(), 2500)
+    }
+}
